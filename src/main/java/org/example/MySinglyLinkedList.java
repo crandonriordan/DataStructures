6ca00c9dedeb00b88c170884d7f60f4ce4e12c65
Crandon rriordan <crandon.riordan@gmail.com>
2020-06-30 19:39:10 -0500

    Align the test assertions to match the API, actual and expectations now match for MySinglyLinkedListTest. Add functions to be used in upcoming Stack example

--- src/main/java/org/example/MySinglyLinkedList.java
+++ src/main/java/org/example/MySinglyLinkedList.java
@@ -30,6 +30,35 @@ public class MySinglyLinkedList<T> {
         return copy.t;
     }
 
+    public void remove(int index) {
+        if(index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int count = 0;
+        Node<T> prev = null;
+        Node<T> copy = head;
+        if(index == 0) {
+            copy = copy.next;
+            head = copy;
+            size--;
+            return;
+        }
+
+        while(count < index) {
+            prev = copy;
+            copy = copy.next;
+            count++;
+        }
+
+        if(prev == null) {
+            return;
+        }
+
+        prev.next = copy.next;
+        size--;
+    }
+
     public void add(T t) {
         if(head == null) {
             head = new Node<T>(t);
@@ -65,13 +94,33 @@ public class MySinglyLinkedList<T> {
         recursiveAdd(node.next, t);
     }
 
+    public T removeTail() {
+        if(size == 0) {
+            return null;
+        }
 
+
+        Node<T> copy = head;
+        while(head != tail) {
+            copy = head;
+            head = head.next;
+        }
+
+        T ret = tail.t;
+        copy.next = null;
+
+        return ret;
+    }
 
     public T getTail() {
         if(tail == null) {
             return null;
         }
         return tail.t;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
 
--- src/test/java/org/example/MySinglyLinkedListTest.java
+++ src/test/java/org/example/MySinglyLinkedListTest.java
@@ -1,4 +1,71 @@
 package org.example;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class MySinglyLinkedListTest {
+
+    @Test
+    public void testAdd() {
+        MySinglyLinkedList<Integer> list = new MySinglyLinkedList<>();
+        assertNull(list.getTail());
+        list.add(5);
+        list.add(7);
+        list.add(9);
+        assertEquals(Integer.valueOf(9), list.getTail());
+    }
+
+    @Test
+    public void testRecursiveAdd() {
+        MySinglyLinkedList<Integer> list = new MySinglyLinkedList<>();
+        list.recursiveAdd(5);
+        list.recursiveAdd(7);
+        list.recursiveAdd(9);
+        assertEquals(Integer.valueOf(9), list.getTail());
+    }
+
+    @Test
+    public void testGetAtIndex() {
+        MySinglyLinkedList<Integer> list = new MySinglyLinkedList<>();
+        list.recursiveAdd(5);
+        list.recursiveAdd(7);
+        list.recursiveAdd(9);
+        assertEquals(Integer.valueOf(9), list.get(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetAtIndexThrowsOutOfBounds() {
+        MySinglyLinkedList<Integer> list = new MySinglyLinkedList<>();
+        list.recursiveAdd(5);
+        list.recursiveAdd(7);
+        list.recursiveAdd(9);
+        assertEquals(Integer.valueOf(9), list.get(3));
+    }
+
+    @Test
+    public void testRemoveAtIndex() {
+        MySinglyLinkedList<Integer> list = new MySinglyLinkedList<>();
+        list.recursiveAdd(5);
+        list.recursiveAdd(7);
+        list.recursiveAdd(9);
+        list.recursiveAdd(11);
+        list.recursiveAdd(12);
+        list.remove(3);
+        assertEquals(Integer.valueOf(12), list.get(3));
+        assertEquals(4, list.getSize());
+    }
+
+    @Test
+    public void testRemoveTail() {
+        MySinglyLinkedList<Integer> list = new MySinglyLinkedList<>();
+        list.recursiveAdd(5);
+        list.recursiveAdd(7);
+        list.recursiveAdd(9);
+        list.recursiveAdd(11);
+        list.recursiveAdd(12);
+        assertEquals(Integer.valueOf(12), list.removeTail());
+    }
+
+
 }
